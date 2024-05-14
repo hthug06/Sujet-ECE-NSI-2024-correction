7dd4505ecf580cb74336ad44b6135ed3a7248be4
ht06 <126491555+hthug06@users.noreply.github.com>
2024-05-14 11:56:20 +0200

    ex1 du 21 fait

--- 24-NSI-21.py
+++ 24-NSI-21.py
@@ -1,12 +1,32 @@
+def recherche_motif(motif, texte):
+    tab = []
+    state = False
+    for i in range(len(texte)-1):
+        state = False
+        for j in range(len(motif)):
+            if texte[i+j] == motif[j]:
+                state = True
+            else:
+                state = False
+                break
+        if state == True:
+            tab.append(i)
+    return tab
+
+print(recherche_motif("ab", ""))
+print( recherche_motif("ab", "cdcdcdcd"))
+print( recherche_motif("ab", "abracadabra"))
+print(recherche_motif("ab", "abracadabraab"))
+
 def parcours(adj, x, acc):
     '''Réalise un parcours en profondeur récursif
     du graphe donné par les listes d'adjacence adj 
     depuis le sommet x en accumulant les sommets
     rencontrés dans acc'''
-    if x ...: 
+    """if x ...: 
         acc.append(x)
         for y in ...: 
-            parcours(adj, ...) 
+            parcours(adj, ...) """
 
 def accessibles(adj, x):
     '''Renvoie la liste des sommets accessibles dans le
