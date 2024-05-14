a9558877a7296fe12e76408c616ac99e8ce8012b
ht06 <126491555+hthug06@users.noreply.github.com>
2024-05-14 11:50:24 +0200

    Ajout ex19 et 20

--- 24-NSI-19.py
+++ 24-NSI-19.py
@@ -1,8 +1,39 @@
+def liste_puissances(a, n):
+    b = a
+    tab = [a]
+    for i in range(n - 1):
+        b = b * a
+        tab.append(b)
+    return tab
+
+
+print(liste_puissances(3, 5))
+print(liste_puissances(-2, 4))
+
+
+def liste_puissances_borne(a, borne):
+    if a < 2: return None
+    b = a
+    tab = []
+    for i in range(borne - 1):
+        if b >= borne:
+            break
+        tab.append(b)
+        b = b * a
+
+    return tab
+
+
+print(liste_puissances_borne(2, 16))
+print(liste_puissances_borne(2, 17))
+print(liste_puissances_borne(5, 5))
+
 dico = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
         "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
         "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17,
         "R": 18, "S": 19, "T": 20, "U": 21, "V": 22,
         "W": 23, "X": 24, "Y": 25, "Z": 26}
+
 
 def codes_parfait(mot):
     """Renvoie un triplet 
@@ -11,12 +42,14 @@ def codes_parfait(mot):
     - code_concatene est le code des lettres du mot concaténées ;
     - mot_est_parfait est un booléen indiquant si le mot est parfait."""
     code_concatene = ""
-    code_additionne = ... 
+    code_additionne = 0
     for c in mot:
-        code_concatene = code_concatene + ... 
-        code_additionne = code_additionne + ... 
+        code_concatene = code_concatene + str(dico[c])
+        code_additionne = code_additionne + dico[c]
     code_concatene = int(code_concatene)
-    mot_est_parfait = ... 
+    mot_est_parfait = code_concatene % code_additionne == 0
     return code_additionne, code_concatene, mot_est_parfait
 
 
+print(codes_parfait("PAUL"))
+print(codes_parfait("ALAIN"))
--- 24-NSI-20.py
+++ 24-NSI-20.py
@@ -1,10 +1,35 @@
+import random
+
+
+def lancer(n):
+    tab = []
+    for i in range(n):
+        tab.append(random.randint(1, 6))
+    return tab
+
+
+def paire_6(tab):
+    nb6 = 0
+    for i in tab:
+        if i == 6:
+            nb6 += 1
+    return True if nb6 >= 2 else False
+
+
+lancer1 = lancer(5)
+print(lancer1)
+print(paire_6(lancer1))
+
+
 def nombre_lignes(image):
     '''renvoie le nombre de lignes de l'image'''
-    return ... 
+    return len(image)
+
 
 def nombre_colonnes(image):
     '''renvoie la largeur de l'image'''
-    return ... 
+    return len(image[0])
+
 
 def negatif(image):
     '''renvoie le negatif de l'image sous la forme
@@ -12,12 +37,13 @@ def negatif(image):
     # on cree une image de 0 aux memes dimensions 
     # que le parametre image
     nouvelle_image = [[0 for k in range(nombre_colonnes(image))]
-         for i in range(nombre_lignes(image))]
+                      for i in range(nombre_lignes(image))]
 
     for i in range(nombre_lignes(image)):
-        for j in range(...): 
-            nouvelle_image[i][j] = ... 
+        for j in range(nombre_colonnes(image)):
+            nouvelle_image[i][j] = 255 - image[i][j]
     return nouvelle_image
+
 
 def binaire(image, seuil):
     '''renvoie une image binarisee de l'image sous la forme
@@ -27,11 +53,20 @@ def binaire(image, seuil):
                       for i in range(nombre_lignes(image))]
 
     for i in range(nombre_lignes(image)):
-        for j in range(...): 
-            if image[i][j] < ... : 
-                nouvelle_image[i][j] = ... 
+        for j in range(nombre_colonnes(image)):
+            if image[i][j] < seuil:
+                nouvelle_image[i][j] = 0
             else:
-                nouvelle_image[i][j] = ... 
+                nouvelle_image[i][j] = 1
     return nouvelle_image
 
 
+img = [[20, 34, 254, 145, 6],
+       [23, 124, 237, 225, 69],
+       [197, 174, 207, 25, 87],
+       [255, 0, 24, 197, 189]]
+
+print(nombre_lignes(img))
+print(nombre_colonnes(img))
+print(negatif(img))
+print( binaire(img,120))
